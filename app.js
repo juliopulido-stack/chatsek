e9b45058e8256ca855ed716ae54a558d06eeb82b
juliopulido-stack <juliopulido@gmail.com>
2026-06-27 12:34:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ try {
             new firebase.appCheck.ReCaptchaV3Provider('6LdyqYMsAAAAAPjGQD-PSjuIjarpCBXO-E-sw9sW'),
             true
         );
-        console.log("ChatSEK v3.3.4 - App Check activado.");
+        console.log("ChatSEK v3.3.5 - App Check activado.");
     }
 } catch (e) {
     console.error("App Check error:", e.message);
